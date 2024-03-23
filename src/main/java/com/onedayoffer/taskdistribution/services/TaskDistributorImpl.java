0af5778fb2388f710b7cd38c5b21883579512218
karandashov <vlad.karandashov.tech@gmail.com>
2024-03-23 13:25:12 +0300

    окончательно причесал

--- src/main/java/com/onedayoffer/taskdistribution/services/TaskDistributorImpl.java
+++ src/main/java/com/onedayoffer/taskdistribution/services/TaskDistributorImpl.java
@@ -11,7 +11,10 @@ import java.util.*;
 @Service
 public class TaskDistributorImpl implements TaskDistributor {
 
+    public static final int MAX_WORKING_TIME = 7 * 60;
+
     /**
+     * Суть алгоритма: взять работника, который освободился раньше всех и (если позволяет время) выдать задачу ему
      * Примерная сложность алгоритма с учётом использования TreeMap:
      * O(n * log(k)), где n - количество заданий, k - сотрудников
      *
@@ -32,9 +35,7 @@ public class TaskDistributorImpl implements TaskDistributor {
         SortedMap<Integer, List<EmployeeDTO>> employeeMap = new TreeMap<>();
         
         // заполняем Map всеми работниками по ключу 0 - так как до этого задач не было
-        employees.forEach(employee -> {
-            putValueToList(employeeMap, 0, employee);
-        });
+        employees.forEach(employee -> putValueByKey(employeeMap, 0, employee));
 
         // сортируем с помощью компаратора (читать описание компаратора)
         tasks.sort(getComparator());
@@ -44,7 +45,7 @@ public class TaskDistributorImpl implements TaskDistributor {
             var startTime = employeeMap.firstKey();
             // высчитываем время, когда освободится работник если возьмёт эту задачу
             var endTime = startTime + task.getLeadTime();
-            if (endTime > 7 * 60) {
+            if (endTime > MAX_WORKING_TIME) {
                 // работник будет занят слишком долго
                 // всех других работников рассматривать нет смысла, так как они освободились также или позже - пропускаем
                 log.warn("Задача {} не будет взята в работу", task);
@@ -54,15 +55,17 @@ public class TaskDistributorImpl implements TaskDistributor {
                 // выдаём задачу
                 selectedEmployee.getTasks().add(task);
                 // кладём сотрудника в Map по новому времени освобождения
-                putValueToList(employeeMap, endTime, selectedEmployee);
+                putValueByKey(employeeMap, endTime, selectedEmployee);
             }
         });
+
+        // по моему алгоритму: если какой-то сотрудник остался недозагруженным - значит мало задач
     }
 
     /**
      * Кладёт в map значение, с учётом того что нужен список
      */
-    private void putValueToList(Map<Integer, List<EmployeeDTO>> map, Integer key, EmployeeDTO employee) {
+    private void putValueByKey(Map<Integer, List<EmployeeDTO>> map, Integer key, EmployeeDTO employee) {
         map.putIfAbsent(key, new ArrayList<>());
         map.get(key).add(employee);
     }
@@ -79,7 +82,7 @@ public class TaskDistributorImpl implements TaskDistributor {
 
     /**
      * Сортируем в первую очередь по приоритетам (сначала 1 потом 10)
-     * При равенстве приоритетов берём в первую очередь длинные задачи
+     * При равенстве приоритетов берём в первую очередь длинные по времени задачи
      *
      * @return компаратор для сортировки
      */
